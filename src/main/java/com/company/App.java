87ef3f71a25a79aabb4e3329ee161cfdcb825362
Vladimir <kozyrev-vova@mail.ru>
2020-01-15 23:39:09 +0300

    Добавил считывание аргументов с командной строки.

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,13 +1,31 @@
 package com.company;
 
-/**
- * Hello world!
- *
- */
+import com.company.dataReader.DataReader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ArrayList<String> inputArgs = new ArrayList<String>();
+        Collections.addAll(inputArgs, args);
+
+        DataReader dataReader = new DataReader(inputArgs);
+        try {
+            dataReader.IsValidInputData();
+            ArrayList<String> links = dataReader.GetLinks();
+            System.out.print("LINKS: ");
+            for (String link: links) {
+                System.out.print(link + " ");
+            }
+            System.out.println();
+            String outputFileName = dataReader.GetOutputFileName();
+            System.out.println("OUTPUT FILE NAME: " + outputFileName);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
--- src/main/java/com/company/dataReader/DataReader.java
+++ src/main/java/com/company/dataReader/DataReader.java
@@ -1,7 +1,50 @@
 package com.company.dataReader;
 
-public class DataReader {
-    public DataReader( String[] args ) {
+import java.util.ArrayList;
 
+public class DataReader {
+    public DataReader(ArrayList<String> inputArgs) {
+        this.inputArgs = inputArgs;
     }
+
+    private int GetIndexAndCheckLinks() {
+        int i = 2;
+        for (; i < inputArgs.size(); i++) {
+            String argument = inputArgs.get(i);
+            if (argument.equals(outWord)) {
+                break;
+            }
+            links.add(inputArgs.get(i));
+        }
+
+        return i;
+    }
+
+    public void IsValidInputData() {
+        String firstArg = inputArgs.get(1);
+        if (!firstArg.equals(filesWord)) {
+            throw new IllegalArgumentException("Input error!");
+        }
+
+        int currIndex = GetIndexAndCheckLinks();
+        if (inputArgs.get(currIndex).equals(outWord)) {
+            outputFileName = inputArgs.get(currIndex + 1);
+        } else {
+            throw new IllegalArgumentException("Input error!");
+        }
+    }
+
+    public ArrayList<String> GetLinks() {
+        return links;
+    }
+
+    public String GetOutputFileName() {
+        return outputFileName;
+    }
+
+    private ArrayList<String> links = new ArrayList<String>();
+    private String filesWord = "--files";
+    private String outWord = "--out";
+    private String outputFileName = "";
+    private ArrayList<String> inputArgs = new ArrayList<String>();
 }
